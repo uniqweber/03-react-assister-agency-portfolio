a35ed78f642e5c27777c47a5fe474f8986d52c42
rayanahmed <thismac@rayanahmed.local>
2025-02-01 02:38:20 +0600

    from input change

--- src/components/SendEmail.jsx
+++ src/components/SendEmail.jsx
@@ -1,4 +1,4 @@
-import { faMailForward, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import {  faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SendEmail = () => {
